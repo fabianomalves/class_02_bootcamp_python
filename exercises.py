108def481cf651e94ffd9eb3c87bb2b1ddf534af
fabianomalves <fabianomalves@proton.me>
2025-01-04 16:41:45 -0300

    Created and resolved the exercise file

---
+++ exercises.py
@@ -0,0 +1,131 @@
+# Exercícios
+
+
+# Inteiros (int)
+
+# Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+number_1 = int(input('Type the first integer number: '))
+number_2 = int(input('Type the second integer number: '))
+sum_numbers = number_1 + number_2
+print(f'The sum of {number_1} and {number_2} is {sum_numbers}.')
+
+# Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+number = int(input('Type an integer number: '))
+division_rest = number % 5
+print(f'The division rest from {number} is {division_rest}.')
+
+# Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+number_1 = int(input('Type the first number: '))
+number_2 = int(input('Type the second number: '))
+product_result = number_1 * number_2
+print(f'The product of {number_1} and {number_2} is {product_result}.')
+
+# Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+number_1 = int(input('Type the number 1: '))
+number_2 = int(input('Type the number 2: '))
+integer_division = number_1 // number_2
+print(f'The integer division of {number_1} by {number_2} is {integer_division}')
+
+# Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+number = int(input('Type a number:'))
+square_number = number ** 2
+print(f'The square of {number} is {square_number}')
+
+
+# Números de Ponto Flutuante (float)
+
+# Escreva um programa que receba dois números flutuantes e realize sua adição.
+number_1 = float(input('Type the first float number: '))
+number_2 = float(input('Type the second float number: '))
+sum_float_numbers = number_1 + number_2
+print(f'The sum of two float numbers({number_1} and {number_2}) is {sum_float_numbers}.')
+
+# Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+number_1 = float(input('Type the first float number: '))
+number_2 = float(input('Type the second float number: '))
+average_float_numbers = (number_1 + number_2) / 2
+print(f'The average number between the two float numbers ({number_1} and {number_2}) is {average_float_numbers}.')
+
+# Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+base_number = float(input('Type the base number: '))
+exponent_number = float(input('Type the exponent number: '))
+power_number = base_number ** exponent_number
+print(f'The power between {base_number} and {exponent_number} is {power_number}.')
+
+# Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+celsius_temperature = float(input('Type a temperature in Celsius:'))
+fahrenheit_temperature = (celsius_temperature * 9/5) + 32
+print(f'The celsius temperature {celsius_temperature} is equivalent to {fahrenheit_temperature} Farenheit.')
+
+# Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+# Import math Library
+import math
+
+PI_CONSTANT = math.pi
+
+radius = float(input('Type the radius from a circle: '))
+circle_area = PI_CONSTANT * (radius ** 2)
+print(f'The area from a circle with a radius of {radius} is {circle_area}.')
+
+
+# Strings (str)
+
+# Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
+text = str(input('Type a text: '))
+upper_text = text.upper()
+print(f'The upper text is: {upper_text}')
+
+# Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+complet_name = str(input('Type your complet name: '))
+lower_complet_name = complet_name.lower()
+print(f'The lower name is {lower_complet_name}')
+
+# Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+phrase = str(input('Type a phrase whit spaces in the end and the biginning: '))
+no_space_phrase = phrase.strip()
+print(f'The phrase without spaces at the beginning and the end  is {no_space_phrase}.')
+
+# Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+date_format_dd_mm_aaaa = str(input('Inform a date with the format dd/mm/aaaa: '))
+day, month, year = date_format_dd_mm_aaaa.split('/')
+print('Day: ', day)
+print('Month: ', month)
+print('Year: ', year)
+
+# Escreva um programa que concatene duas strings fornecidas pelo usuário.
+first_string = str(input('Type a part of the text: '))
+second_string = str(input('Type other text part: '))
+concatenated_string = f'{first_string} {second_string}'
+print(f'The concatenated string is: {concatenated_string}')
+
+
+# Booleanos (bool)
+
+# Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
+value_1 = True
+value_2 = False
+result_and_two_values = value_1 and value_2
+print(result_and_two_values)
+
+# Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
+value_1 = True
+value_2 = False
+result_or_two_values = value_1 or value_2
+print(result_or_two_values)
+
+# Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
+value_boolean = False
+inverted_value = not value_boolean
+print(inverted_value)
+
+# Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
+number_1 = 5
+number_2 = 10
+comparison_equal_two_numbers = number_1 == number_2
+print(comparison_equal_two_numbers)
+
+# Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
+number_1 = 6
+number_2 = 11
+comparison_different_two_numbers = number_1 != number_2
+print(comparison_different_two_numbers)
